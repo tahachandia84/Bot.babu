3926b468cb256833a5b40b5b1fc3b2dfdaabe5bd
KRISHNA BOT MASTER <ravikumar630344222@gmail.com>
2025-10-16 10:07:04 +0530

    Update pair.js

--- models/commands/pair.js
+++ models/commands/pair.js
@@ -1,73 +1,46 @@
-const PREM_BABU= "PREM-PROJECT"; // Original credit
-
 module.exports.config = {
   name: "pair",
   version: "1.0.0", 
   hasPermssion: 0,
-  credits: PREM_BABU,
-  description: "THIS BOT WAS MADE BY MR PREM BABU",
-  commandCategory: "LOVE PAIRING",
-  usePrefix: false,
-  usages: "PREFIX", 
-  cooldowns: 5
+  credits: "D-Jukie (Xuyên get)",
+  description: "Pairing",
+  commandCategory: "Love", 
+  usages: "pair", 
+  cooldowns: 15
 };
+module.exports.run = async function({ api, event,Threads, Users }) {
+        const axios = global.nodemodule["axios"];
+        const fs = global.nodemodule["fs-extra"];
 
-module.exports.run = async function({ api, event, Threads, Users }) {
-  const axios = global.nodemodule["axios"];
-  const fs = global.nodemodule["fs-extra"];
+        var { participantIDs } =(await Threads.getData(event.threadID)).threadInfo;
+        var tle = Math.floor(Math.random() * 101);
+        var namee = (await Users.getData(event.senderID)).name
+        const botID = api.getCurrentUserID();
+        const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
+        var id = listUserID[Math.floor(Math.random() * listUserID.length)];
+        var name = (await Users.getData(id)).name
+        var arraytag = [];
+                arraytag.push({id: event.senderID, tag: namee});
+                arraytag.push({id: id, tag: name});
 
-  var { participantIDs } = (await Threads.getData(event.threadID)).threadInfo;
-  var tle = Math.floor(Math.random() * 101);
-  var namee = (await Users.getData(event.senderID)).name
-  const botID = api.getCurrentUserID();
-  const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
-  var id = listUserID[Math.floor(Math.random() * listUserID.length)];
-  var name = (await Users.getData(id)).name
-  var arraytag = [];
-  arraytag.push({id: event.senderID, tag: namee});
-  arraytag.push({id: id, tag: name});
+  
+        let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
+            fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
 
-  // Check if credit has been modified
-  if (module.exports.config.credits !== PREM_BABU) {
-    return api.sendMessage("DON'T CHANGE CREDIT! PREM-PROJECT FUCK YOUR MOTHER AND SISTER", event.threadID, event.messageID);
-  }
+        let gifLove = (await axios.get( `https://i.postimg.cc/SxKNnBht/MBETCWy.gif`, { responseType: "arraybuffer" } )).data; 
+            fs.writeFileSync( __dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8") );
 
-  // Array of Imgur gif links
-  const imgurGifs = [
-    "https://i.imgur.com/41ogamD.gif",
-    "https://i.imgur.com/qVpA7RB.gif",
-    "https://i.imgur.com/OJ6uIXY.gif",
-    "https://i.imgur.com/3qSoi8K.gif",
-    "https://i.imgur.com/EO5EtJp.gif",
-    "https://i.imgur.com/fP5kc0h.gif"
-    // Add more Imgur gif links as needed
-  ];
+        let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
+            fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
 
-  // Select a random Imgur gif link
-  const randomImgurLink = imgurGifs[Math.floor(Math.random() * imgurGifs.length)];
+        var imglove = [];
+              
+              imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
+              imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
+              imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
 
-  // Download the gif from the selected Imgur link
-  const gifData = (await axios.get(randomImgurLink, { responseType: "arraybuffer" })).data; 
-  fs.writeFileSync(__dirname + "/cache/giflove.png", Buffer.from(gifData, "utf-8"));
-
-  // Download avatars
-  let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
-  fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
-
-  let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
-  fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
-
-  // Prepare attachments
-  var imglove = [];
-  imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-  imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-  imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
-
-  var msg = {
-    body: `LO MIL GAYA AAP KA JIVAN SATHI 🙂🖐️\nAB BAR BAR MAT BOLNA SETTING KARVANE KI 😒👈\nAAP DONO KA PYAR 👉 ${tle}% है\n`+namee+" "+"💖"+" "+name, 
-    mentions: arraytag, 
-    attachment: imglove
-  };
-
-  return api.sendMessage(msg, event.threadID, event.messageID);
+        var msg = {body: `‎🌸💖 𝗢𝘄𝗻𝗲𝗿 ➻ 𝐊𝐚𝐛𝐮𝐭𝐫𝐢 𝐁𝐞𝐛𝐲 🌸🍒\n✧═════════•❁❀❁•═════════✧\n𝗖𝗿𝗲𝗮𝘁𝗲 ➻ 🇰‌🇷‌🇮‌🇸‌🇭‌🇳‌🇦‌😍😘🌸\n✧═════════•❁❀❁•═════════✧\n[•||•●•||•┼┼──🌸𝐓ʋ𝗺𝘀𝗮 𝐊❍ıı 𝐏ɣ𝗮𝗮ɽ𝗮 𝐊❍ıı  𝐌𝗮𝘀❍𝗺 𝐍𝗮Ħıı 𝐇𝐚ıı•||•🐬•||•]]
+🌹\n✧═════════•❁❀❁•═════════✧\n🌹
+[•||•●•||•┼┼──🌸🌿𝐓ʋ𝗺 𝐉𝗮𝗮η  𝐇❍ 𝐌ƏɽƏ 𝐓ʋ𝗺ĦƏ 𝐌𝗮ɭʋ𝗺 𝐍𝗮Ħıı 𝐇𝐚ıı•||•🌸•||• ]]\n✧═════════•❁❀❁•═════════✧\n𝗟𝗼𝘃𝗲 𝗥𝗮𝘁𝗶𝗼 :❣️[${tle}%]👈❤️‍🩹💫\n✧═════════•❁❀❁•═════════✧\n🍒😘 ➻ ${namee} ️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ \n\n🦋💞🦋\n\n [ ${name} ] \n✧═════════•❁❀❁•═════════✧\n\nℂ𝕆𝔻𝔼 𝔹𝕐 :- 💖 𝗞𝗥𝗜𝗦𝗛𝗡𝗔 𝗕𝗔𝗕𝗨`, mentions: arraytag, attachment: imglove}
+                return api.sendMessage(msg, event.threadID, event.messageID)
 }
